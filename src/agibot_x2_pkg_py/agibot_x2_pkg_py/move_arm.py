30208a7e596cb1195fb95f19c8a171c18fb9f5db
Cate <cate10aprile@gmail.com>
2026-08-08 15:16:49 +0200

    Fixed gaz resources and movement script

--- src/agibot_x2_pkg_py/agibot_x2_pkg_py/move_arm.py
+++ src/agibot_x2_pkg_py/agibot_x2_pkg_py/move_arm.py
@@ -6,8 +6,11 @@ class JointAnglePublisher(Node):
     def __init__(self):
         super().__init__(
             'initial_pose_publisher', 
-            automatically_declare_parameters_from_overrides=True # <--- Consente di leggere use_sim_time
+            #automatically_declare_parameters_from_overrides=True # <--- Consente di leggere use_sim_time
         )
+
+        #Dichiara e abilita il parametro use_sim_time se passato da launch/CLI
+        self.declare_parameter('use_sim_time', True)
         
         self.publisher = self.create_publisher(
             JointTrajectory, 
@@ -32,7 +35,14 @@ class JointAnglePublisher(Node):
 
     def send_joint_angles(self):
         trajectory_command = JointTrajectory()
-        trajectory_command.header.stamp = self.get_clock().now().to_msg()
+
+        #trajectory_command.header.stamp = self.get_clock().now().to_msg()
+        # SOLUZIONE CHIAVE: Lasciando stamp a 0 (default), 
+        # il controller esegue la traiettoria "IMMEDIATAMENTE", 
+        # evitando che venga scartata per problemi di sincronizzazione oraria.
+        trajectory_command.header.stamp.sec = 0
+        trajectory_command.header.stamp.nanosec = 0
+        
         trajectory_command.joint_names = self.joint_names
 
         point = JointTrajectoryPoint()
@@ -72,64 +82,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-'''
-import rclpy
-from rclpy.node import Node
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-
-class JointAnglePublisher(Node):
-    def __init__(self):
-        super().__init__('initial_pose_publisher')
-        # Create a publisher for the '/arm_controller/joint_trajectory' topic
-        self.publisher = self.create_publisher(JointTrajectory, '/left_arm_controller/joint_trajectory', 10)
-
-        # Create the JointTrajectory message
-        self.trajectory_command = JointTrajectory()
-        joint_names = [
-            'left_shoulder_pitch_joint',
-            'left_shoulder_roll_joint',
-            'left_shoulder_yaw_joint',
-            'left_elbow_joint',
-            #'left_wrist_pitch_joint',
-            #'left_wrist_roll_joint',
-            'left_wrist_yaw_joint'
-        ]
-        self.trajectory_command.joint_names = joint_names
-
-        point = JointTrajectoryPoint()
-        #['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_joint', 'left_finger_joint', 'right_finger_joint']
-        #point.positions = [0.0, 0.91, 1.37, -0.63, 0.3, 0.3]
-        #point.velocities = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        
-        # Assegniamo una posizione (in radianti) per ciascuno dei 7 giunti del braccio
-        # Ad esempio muoviamo la spalla (pitch) a 0.5 rad e il gomito a -0.5 rad
-        point.positions = [0.5, 0.0, 0.0, -0.5, 0.0,]# 0.0, 0.0]
-        point.velocities = [0.0] * len(self.trajectory_command.joint_names)
-        point.time_from_start.sec = 2  # Tempo per completare il movimento (es. 2 secondi)
-
-        self.trajectory_command.points = [point]
-
-        # Publish the message
-        self.get_logger().info('Publishing joint angles...')
-
-    def send_joint_angles(self):
-        while rclpy.ok():
-            self.publisher.publish(self.trajectory_command)
-            rclpy.spin_once(self, timeout_sec=0.1)
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = JointAnglePublisher()
-    try:
-        node.send_joint_angles()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
-'''
